5402164324a2c281e9e487d3e169bfacc6063868
Shanen Cross <shanencross@yahoo.com>
2021-05-18 21:00:38 -0700

    Add Create view and action
    Co-authored-by: Cat Denton <Cat-Denton@users.noreply.github.com>
    Co-authored-by: Karlson Drendel <kdrendel99@users.noreply.github.com>

--- BestRestaurants/Controllers/RestaurantsController.cs
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -3,12 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using BestRestaurants.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace BestRestaurants.Controllers
 {
   public class RestaurantsController : Controller
   {
-    BestRestaurantsContext _db;
+    private readonly BestRestaurantsContext _db;
 
     public RestaurantsController(BestRestaurantsContext db)
     {
@@ -17,8 +19,27 @@ namespace BestRestaurants.Controllers
 
     public ActionResult Index()
     {
-      List<Restaurant> restaurantList = _db.Restaurants.Include(restaurant => restaurant.Cuisine).ToList();
-      return View(restaurantList);
+      List<Restaurant> model = _db.Restaurants.Include(restaurant => restaurant.Cuisine).ToList();
+      Console.WriteLine(model.Count);
+      foreach (Restaurant restaurant in model)
+      {
+        Console.WriteLine(restaurant.Name);
+      }
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
   }
